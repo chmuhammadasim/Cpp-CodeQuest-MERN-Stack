05a1f55f3cfeb68fb68df34647907183b8f471f7
chmuhammadasim <boybrave82@gmail.com>
2024-07-25 14:05:49 +0500

    chore: Add Dashboard page with progress fetch

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Navbar from '../components/Navbar';
+
+const Dashboard = () => {
+  const [progress, setProgress] = useState({});
+
+  useEffect(() => {
+    const fetchProgress = async () => {
+      const token = localStorage.getItem('token');
+      const response = await axios.get('http://localhost:5000/api/users/progress', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      setProgress(response.data.progress);
+    };
+    fetchProgress();
+  }, []);
+
+  return (
+    <div>
+      <Navbar />
+      <h1>Dashboard</h1>
+      <pre>{JSON.stringify(progress, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default Dashboard;
